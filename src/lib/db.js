b6ef7dc17ef260bcfaafb00fd6efbc21257c698a
fasil <fasilsebsibe27@gmail.com>
2025-05-21 15:25:55 +0300

    add import auth route  and connect database

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const connectDB = async ()=>{
+ export const connectDB = async ()=>{
     try {
        const conn = await mongoose.connect(process.env.MONGODB_URL);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
