180b6df4844cd40c8206ff437ef0fa3882781ac7
fasil <fasilsebsibe27@gmail.com>
2025-05-26 21:45:11 +0300

    fix and edit dialoge

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import  {connectDB} from './lib/db.js';
-import authRoutes from './routes/auth.route.js';
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
- dotenv.config();
-const app = express()
+dotenv.config();
+const app = express();
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 const PORT = process.env.PORT || 5000;
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
   connectDB();
-}
-);
+});
